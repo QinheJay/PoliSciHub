5d27df27daabba9608d139d74abe6b72c9068838
Rui <yehruei@163.com>
2024-11-14 12:15:42 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/router'
-import 'vfonts/RobotoSlab.css'
 import naive from 'naive-ui'
+import 'vfonts/RobotoSlab.css'
 
 createApp(App).use(naive).use(router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/PoliSciHub/', // 替换为您的仓库名称
+  base: '/PoliSciHub/',
   build: {
     outDir: 'dist'
   }
