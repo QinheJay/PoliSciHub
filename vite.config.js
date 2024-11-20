05f8b679be90c454ff162d883b29fda466a03f9b
Rui <yehruei@163.com>
2024-11-20 15:42:47 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/PoliSciHub/',
+  base: '/',
   build: {
     outDir: 'dist'
   }
